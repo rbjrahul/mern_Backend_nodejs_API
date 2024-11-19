9719cd9cb30216a93ae5ebb29642aa1530f962cb
rbjrahul <rahulbj.rj@gmail.com>
2024-11-19 15:17:57 +0530

    server.js security update

--- server.js
+++ server.js
@@ -48,7 +48,6 @@ app.use((error, req, res, next) => {
 mongoose
   .connect(
     `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.sn6fu.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`
-    //`mongodb+srv://rjrahul:4ImjFpjtRSRG1bHc@cluster0.sn6fu.mongodb.net/MERN?retryWrites=true&w=majority&appName=Cluster0`
   )
   .then(() => {
     app.listen(process.env.PORT || port);
